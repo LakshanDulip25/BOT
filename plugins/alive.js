ec9e0f54454dca94de34fb2e7187c74e56ce898b
LakshanDulip25 <lxn000000@gmail.com>
2025-03-21 12:12:25 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -8,8 +8,10 @@ cmd({
 },
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+    await robin.sendMessage(from,{sticker: { url : "https://github.com/LakshanDulip25/Bothelper/raw/refs/heads/main/sticker/alive.webp" },package: 'LXN'},{ quoted: mek })
     await robin.sendPresenceUpdate('recording', from);
     await robin.sendMessage(from, { audio: { url: "https://github.com/LakshanDulip25/Bothelper/raw/refs/heads/main/audio/alive.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
